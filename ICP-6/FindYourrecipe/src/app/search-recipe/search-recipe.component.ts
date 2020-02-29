8efa2a63c911c9c8f2ff7981d157a9e07d7eaed4
madhurisarda <54335408+madhurisarda@users.noreply.github.com>
2020-02-29 15:51:11 -0600

    icp-6

--- ICP-6/FindYourrecipe/src/app/search-recipe/search-recipe.component.ts
+++ ICP-6/FindYourrecipe/src/app/search-recipe/search-recipe.component.ts
@@ -20,20 +20,15 @@ export class SearchRecipeComponent implements OnInit {
   geolocationPosition: any;
 
   recepieApi = 'https://api.edamam.com/search?q=';
-  recepieAppid = '&app_id=454fe335';
-  recepieKey = '&app_key=0773cc089b069e8196a3ad56c6374a42';
+  recepieAppid = '&app_id=ba18fdc5';
+  recepieKey = '&app_key=588b400e488e39a56644f2a2e8094f12';
 
-  placesApi = 'https://api.foursquare.com/v2/venues/search?';
-  clientdId = 'client_id=AZ5YEHLFZWTZJQAO0YPRGLGIIVFQOS3MRSPFHDOZRQ1XU1PD';
-  clientSecret = '&client_secret=1HKYC2PGOKIAYIQAXDCGZOSAADRLRVND0UMF5VLRA1GVDJ4A';
-  version = '&v=20180323';
-  near1 = '&near=';
-  query = '&query=';
+
   constructor(private _http: HttpClient) {
   }
 
   ngOnInit() {
-
+//for getting the users current location
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.geolocationPosition = position;
@@ -41,14 +36,14 @@ export class SearchRecipeComponent implements OnInit {
         this.currentLong = position.coords.longitude;
       });
   }
-
+//this method get the locations and we are only storing the label, icons and url and showing it on the html page.
   getVenues() {
-
     this.recipeValue = this.recipes.nativeElement.value;
     this.placeValue = this.places.nativeElement.value;
-
+    //for the recipes we are limiting the results to 5 recipes
     if (this.recipeValue !== null) {
-      this._http.get( this.recepieApi + this.recipeValue + this.recepieAppid + this.recepieKey).subscribe((result: any) => {
+      this._http.get("https://api.edamam.com/search?q=" +this.recipeValue + "&app_id=ba18fdc5&app_key=588b400e488e39a56644f2a2e8094f12&to=5")
+          .subscribe((result: any) => {
         console.log(result);
         this.recipeList = Object.keys(result.hits).map(function (k) {
           const i = result.hits[k].recipe;
@@ -58,10 +53,10 @@ export class SearchRecipeComponent implements OnInit {
       });
 
     }
-
+//for the location search we are limiting the location to 8 results
     if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
-      this._http.get( this.placesApi  + this.clientdId + this.clientSecret + this.version
-        + this.near1 + this.placeValue).subscribe((result: any) => {
+      this._http.get("https://api.foursquare.com/v2/venues/search?client_id=R0DXXKM5BTJMRK3HRXMCRZ3QK3UG3LM1AUDLHDNEBQZRCYTZ" +
+          "&client_secret=IOWJRGIHCVO2UIYKFLPQCRL3WDYIEDX1FJVLET2J3XMRHLHT&v=20180323&limit=8&near=" +this.placeValue).subscribe((result: any) => {
         console.log(result);
         this.venueList = Object.keys(result.response.venues).map(function (k) {
           const i = result.response.venues[k];
